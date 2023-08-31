ee9430de787b7a9016c062575c44b5319516f175
Lubomir Vitol <VitolLub@gmail.com>
2023-08-31 20:34:35 +0300

    Delete test_module directory

--- test_module/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# import pydevd_pycharm
-# pydevd_pycharm.settrace('192.168.178.27', port=5234, stdoutToServer=True, stderrToServer=True)
-
-from . import models
-from . import controllers
-
-
