e170145a7d9207f080e0e8d67d29424213e9aa0c
vitollub <vitollub@gmail.com>
2023-09-19 16:18:58 +0300

    Fixed problem with Vendors

--- scheduled_date/models/product_purchase_list.py
+++ scheduled_date/models/product_purchase_list.py
@@ -16,13 +16,18 @@ class ProductPurchaseList(models.Model):
     )
 
     def _compute_purchase_order_ids(self):
+        # if
         for product in self:
+            # search data from purchase.order model
             purchase_orders = self.env['purchase.order'].search([
                 ('order_line.product_id.default_code', '=', product.default_code),
                 ('state', 'in', ['purchase', 'to approve','sent','draft']),  # Filter only completed or ongoing orders
             ])
 
-            # select purchase order data
+            # filtered partner_id from purchase_orders if purchase_orders.partner_id not in product.seller_ids
+            purchase_orders = purchase_orders.filtered(lambda r: r.partner_id in product.seller_ids.name)
+
+            # assign purchase_orders to product.purchase_order_ids
             product.purchase_order_ids = purchase_orders
 
 
