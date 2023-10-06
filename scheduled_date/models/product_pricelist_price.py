493436f2d3bc8a2f86a6e1d511a74dba7deef965
vitollub <vitollub@gmail.com>
2023-10-06 13:14:49 +0300

    Added actual_price to price_list_item. Last working version

--- scheduled_date/models/product_pricelist_price.py
+++ scheduled_date/models/product_pricelist_price.py
@@ -33,12 +33,21 @@ class ProductTemplate(models.Model):
                     pricelist = self.env['product.pricelist'].browse(product.pricelist_id.id)
 
                     # get product data for current product
+                    # product.product_id.id is parent product id not variant id(product.id)
                     product_res = self.env['product.product'].browse(product.product_id.id)
 
                     # get partner id for current product
                     uom = product_res.uom_id.id
 
-                    # get actual price for current product
+
+                    '''
+                    get actual price for current product
+                    
+                    get_product_price method has 3 parameters
+                    product_res - data from product.product by parent product id
+                    False - System or Current user. By default is 1 (System), but some DB has no user with id 1, so set in False
+                    False - Timeout. By default is False, but if you need storage current field, then set up timeout.
+                    '''
                     actula_price = pricelist.with_context(uom=uom).get_product_price(product_res, False, False)
 
                     # assign actual price to current product
